8da5bf816278861a6ca0447c3c377e3ae53df544
daklab <david.arthur.knowles@gmail.com>
2020-01-29 22:25:20 -0500

    fix bib

--- publications/build_pubs.sh
+++ publications/build_pubs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias  jabref='java -jar /Applications/JabRef.app/Contents/java/app/JabRef-4.3.1.jar'
+
 # jabref -i publications.bib -m "((entrytype=inproceedings) and (keywords=mlstats))",ml_conf.html,website -n &
 jabref -i publications.bib -m "keywords=mlstats",ml.html,website -n &
 jabref -i publications.bib -m "keywords=genetics",genetics.html,website -n &
