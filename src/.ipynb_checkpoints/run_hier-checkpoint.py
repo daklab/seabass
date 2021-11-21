f7b5558b448b45e4cb83054400367a2ca248eb06
Karin Isaev <kisaev@pe2dc5-002.c.nygenome.org>
2021-11-21 17:33:53 -0500

    run seabass on different input files with R1

---
+++ src/.ipynb_checkpoints/run_hier-checkpoint.py
@@ -0,0 +1,70 @@
+import seabass
+import seabass_hier
+import torch
+
+torch.set_num_threads(20)
+
+import matplotlib.pyplot as plt
+import sys
+import pyro
+import pandas as pd
+from pathlib import Path
+import numpy as np
+from importlib import reload  # Python 3.4+
+reload(seabass) 
+reload(seabass_hier)
+
+pyro.clear_param_store()
+
+# load data
+data_dir = Path("/gpfs/commons/groups/knowles_lab/Cas13Karin/analysis/")
+dat = pd.read_csv(data_dir / "Cas13_essential_arm_foldchanges.txt", sep = "\t")
+
+dat = dat.rename(columns={"Gene": "junction", 
+                          "gene.name" : "gene", 
+                          "value": "logFC"})
+plt.hist(dat.logFC,100)
+
+data = seabass_hier.HierData.from_pandas(dat) 
+
+# for reproducibility
+pyro.set_rng_seed(101)
+
+model, guide, losses = seabass_hier.fit(data, iterations=500)
+
+posterior_stats = seabass.get_posterior_stats(model, guide, data)
+
+# check convergence
+plt.figure(figsize=(9,4))
+plt.plot(losses)
+plt.ylabel("ELBO")
+plt.xlabel("Iterations")
+plt.show()
+
+# plot estimates
+plt.figure(figsize=(9,8))
+plt.subplot(221)
+plt.hist( posterior_stats["guide_efficacy"]["mean"], 30 )
+plt.xlabel('guide_efficacy')
+plt.subplot(222)
+plt.hist( posterior_stats["junction_efficacy"]["mean"], 30 )
+plt.xlabel('junction_efficacy')
+plt.subplot(223)
+plt.hist( posterior_stats["gene_essentiality"]["mean"], 30 )
+plt.xlabel('gene_essentiality')
+plt.show() 
+
+plt.scatter(posterior_stats["guide_efficacy"]["mean"], 
+            posterior_stats["guide_efficacy"]["std"], 
+            alpha = 0.05)
+plt.xlabel('guide_efficacy mean') 
+plt.ylabel('guide_efficacy std') 
+plt.show() 
+
+plt.scatter(posterior_stats["junction_efficacy"]["mean"], 
+            posterior_stats["junction_efficacy"]["std"], 
+            alpha = 0.05)
+plt.xlabel('junction_efficacy mean') 
+plt.ylabel('junction_efficacy std') 
+plt.show() 
+
